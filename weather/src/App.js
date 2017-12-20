964e120164a1d366e50456977162e9ae5d05caa8
Bagaskoro Meyca <bagas_mdp@yahoo.co.id>
2017-12-20 10:48:45 +0700

    unfinished

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,18 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import $ from 'jquery';
+import Projects from './Components/Weather';
+import Todos from './Components/City';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      weather: [],
+      city:[]
+    }
+  }
+
+  getCity(){
+    let API_KEY = 'AIzaSyAIQ1DQlAbEevd6GAfBjFghj-DMUm2MRf0';
+    $.ajax({
+      url: 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=Vict&types=(cities)&language=pt_BR&key=' + API_KEY,
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({city: data}, function(){
+          console.log(this.state);
+        });
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+      }
+    });
+  }
+
+  getWeather(){
+    $.ajax({
+      url: 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=Vict&types=(cities)&language=pt_BR&key=' + API_KEY,
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({weather: data}, function(){
+          console.log(this.state);
+        });
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+      }
+    });
+  }
+
+  componentWillMount(){
+    this.getCity();
+    this.getWeather();
+  }
+
+  componentDidMount(){
+    this.getWeather();
+  }
+
+  handleAddProject(project){
+    let city = this.state.city;
+    projects.push(city);
+    this.setState({projects:projects});
+  }
+
+  handleDeleteProject(id){
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState({projects:projects});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AddProject addProject={this.handleAddProject.bind(this)} />
+        <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)} />
+        <hr />
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
---
+++ weather/src/components/SelectCity.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+class SelectCity extends Component {
+  constructor(){
+    super();
+    this.state = {
+      newCity:{}
+    }
+  }
+
+  static defaultProps = {
+    categories: ['Web Design', 'Web Development', 'Mobile Development']
+  }
+
+  handleSubmit(e){
+    if(this.refs.title.value === ''){
+      alert('Title is required');
+    } else {
+      this.setState({newProject:{
+        id: uuid.v4(),
+        title: this.refs.title.value,
+        category: this.refs.category.value
+      }}, function(){
+        //console.log(this.state);
+        this.props.addProject(this.state.newProject);
+      });
+    }
+    e.preventDefault();
+  }
+
+  render() {
+    let categoryOptions = this.props.categories.map(category => {
+      return <option key={category} value={category}>{category}</option>
+    });
+    return (
+      <div>
+        <h3>Add Project</h3>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <div>
+            <label>Title</label><br />
+            <input type="text" ref="title" />
+          </div>
+          <div>
+            <label>Category</label><br />
+            <select ref="category">
+              {categoryOptions}
+            </select>
+          </div>
+          <br />
+          <input type="submit" value="Submit" />
+          <br />
+        </form>
+      </div>
+    );
+  }
+}
+
+AddProject.propTypes = {
+  categories: React.PropTypes.array,
+  addProject: React.PropTypes.func
+}
+
+export default SelectCity;
